a0b33f44d3335c32faeaeac7a23910a4aa0c5017
jtakumi <92016292+jtakumi@users.noreply.github.com>
2021-11-29 21:16:24 +0900

    Add files via upload

---
+++ AllDataType.php
@@ -0,0 +1,13 @@
+<?php
+     $arr=array(1,2,3,4);
+     $number=40;
+     $apple="apple";
+     $var=true;
+     $nul=NULL;
+
+     echo gettype($arr) . PHP_EOL;
+     echo gettype($number) . PHP_EOL;
+     echo gettype($apple) . PHP_EOL;
+     echo gettype($var) . PHP_EOL;
+     echo gettype($nul) . PHP_EOL;
+     ?>
---
+++ OddEven.php
@@ -0,0 +1,5 @@
+<?php
+     var_dump($argv);
+    if($argv[1]%2==1)echo "入力値は奇数です" . PHP_EOL;
+    else echo "入力値は偶数です" . PHP_EOL;
+    ?>
